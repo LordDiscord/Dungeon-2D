86c21dca8a0e7b2b395e0e7cad7a2e00de535c1a
Arkantes <160763182+Arkantes@users.noreply.github.com>
2024-05-14 18:11:26 +0200

    He creado 5 stats
    Mejor iniciarlas en las subclases correspondientes (pj = 15~20) y un goblin 7 por ejemplo

--- Dungeon 2D/Assets/Scripts/Character.cs
+++ Dungeon 2D/Assets/Scripts/Character.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+
+    private int vida;
+    private int armadura;
+    private int destreza;
+    private int inteligencia;
+    private int iniciativa;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    
 
     // Update is called once per frame
     void Update()
