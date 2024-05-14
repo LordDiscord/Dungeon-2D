7244dd78a04c296749734852bdfcad693caa019d
Arkantes <160763182+Arkantes@users.noreply.github.com>
2024-05-14 21:51:47 +0200

    Corregido error
    He simplificado cosas

--- Dungeon 2D/Assets/Scripts/MainCharacter.cs
+++ Dungeon 2D/Assets/Scripts/MainCharacter.cs
@@ -6,22 +6,14 @@ using UnityEngine.Video;
 public class MainCharacter : Character
 {
     
-    public MainCharacter(bool mago) 
+    public MainCharacter() 
     {
         vida = Random.Range(15, 21);
         armadura = 10;
         destreza = 10;
         inteligencia = 10;
         iniciativa = destreza / 2 + tirarD6(); //posible cambio (6~12)
-
-        if(mago)
-        {
-            vida -= 2;
-            armadura -= 4;
-            destreza -= 3;
-            inteligencia += 5;
-        }
-
+        
     }
 
     // Start is called before the first frame update
