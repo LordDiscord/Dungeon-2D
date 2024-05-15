34872ddd7f61e5be4837244bcb3742006758d04b
Arkantes <160763182+Arkantes@users.noreply.github.com>
2024-05-15 18:38:21 +0200

    +1 escena, clase guerrero creada

---
+++ Dungeon 2D/Assets/Scripts/Guerrero.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guerrero : MainCharacter
+{
+    // Start is called before the first frame update
+    public Guerrero()
+    {
+        vida += vida + 5;
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
